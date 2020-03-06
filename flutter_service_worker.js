6c8c3eca5614cb7617456bcb5e7642138ae4ae57
meokg456 <meokg456@gmail.com>
2020-03-06 18:07:32 +0700

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/assets/android/app/src/main/res/drawable/pokemon.png": "ce559c0c21410a5bb97508801fbff68c",
 "/assets/AssetManifest.json": "9593000e417a40f0abee5dd6bda826be",
-"/assets/data/dex.json": "dc7125a1b76fed1a564618c116b8d8e6",
+"/assets/data/dex.json": "d38dad6be39d67f5a9468d2134e9c0a3",
 "/assets/FontManifest.json": "9980c2bf72478075d5ef12b9b9adc1c2",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/fonts/pokemon_hollow.ttf": "6e5393a64ca23c455e8568d409e82da6",
@@ -14,10 +14,13 @@ const RESOURCES = {
 "/assets/images/kanto.png": "685773ad18a4e14f24f773fce6580305",
 "/assets/images/sinnoh.png": "ae6d538fc3176b24c1f221ccedcf3a28",
 "/assets/images/unova.png": "cabf06ced2e9c06d531edb1db61aed44",
-"/assets/LICENSE": "0066ffbf0540abb46355a090c265ea44",
-"/index.html": "25bdf0c2a3be2492ae91c298649ea518",
-"/main.dart": "e68577c5c559988aafd3fad5d6f9451b",
-"/main.dart.js": "19d0451573416af78eb61a1dea6901ea"
+"/assets/LICENSE": "8383d6bfd2695facc0a24f83901b1d89",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/index.html": "d0248dccd99dff9cb61ec786c72828d7",
+"/main.dart.js": "19d0451573416af78eb61a1dea6901ea",
+"/manifest.json": "2038593c0fc25efb758a8d10205c1f9a"
 };
 
 self.addEventListener('activate', function (event) {
