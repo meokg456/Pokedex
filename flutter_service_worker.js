d44827b6b5394602e85f9220693f46eb02712dff
meokg456 <meokg456@gmail.com>
2020-03-06 19:37:20 +0700

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/.dart_tool/package_config.json": "b7bc0088e600406dde9e6223c6b87e30",
 "/assets/android/app/src/main/res/drawable/pokemon.png": "ce559c0c21410a5bb97508801fbff68c",
-"/assets/AssetManifest.json": "9593000e417a40f0abee5dd6bda826be",
+"/assets/AssetManifest.json": "e11663fb7f8e4d870bcc9165f2309ce0",
 "/assets/data/dex.json": "d38dad6be39d67f5a9468d2134e9c0a3",
-"/assets/FontManifest.json": "9980c2bf72478075d5ef12b9b9adc1c2",
+"/assets/FontManifest.json": "29d1660c764bc7beb02690657aba4440",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/fonts/pokemon_hollow.ttf": "6e5393a64ca23c455e8568d409e82da6",
 "/assets/fonts/pokemon_solid.ttf": "36d26dbcd032a9d91d891c9f22dfdec6",
@@ -15,12 +15,14 @@ const RESOURCES = {
 "/assets/images/kanto.png": "685773ad18a4e14f24f773fce6580305",
 "/assets/images/sinnoh.png": "ae6d538fc3176b24c1f221ccedcf3a28",
 "/assets/images/unova.png": "cabf06ced2e9c06d531edb1db61aed44",
-"/assets/LICENSE": "8383d6bfd2695facc0a24f83901b1d89",
+"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "0938aff7c80c1bd3a6309fd684d54951",
-"/main.dart.js": "19d0451573416af78eb61a1dea6901ea",
-"/manifest.json": "fe81f155b8c66222b7c5008e685d0a2a",
+"/index.html": "49cf51e544bdba606f1e7ff11f4c74c6",
+"/main.dart.js": "c3cd23db8e2b4102fa11491ded53a446",
+"/manifest.json": "2038593c0fc25efb758a8d10205c1f9a",
 "/packages/$sdk/dev_compiler/kernel/amd/dart_sdk.js": "02c7ac0ebc9ea083c55c5e4c4f43acb4",
 "/packages/$sdk/dev_compiler/kernel/amd/require.js": "eb0ef9ae5ed627559fa95dce2937ac06",
 "/packages/$sdk/dev_compiler/kernel/common/dart_sdk.js": "4cf58a3f7baa3705a429052a2acd30bd",
@@ -291,8 +293,7 @@ const RESOURCES = {
 "/packages/test/src/runner/browser/static/host.css": "b73ec42b1e5350638bc4fc5e23eb3d68",
 "/packages/test/src/runner/browser/static/host.dart.js": "c39c6003e00668a1e4c761c50858b48b",
 "/packages/test/src/runner/browser/static/index.html": "e855595491a1a2a7695f48b82da58a4e",
-"/packages/_fe_analyzer_shared/src/parser/parser.md": "9cc5da5eb7d930990cc9b1f176f29ed0",
-"/pokemon.png": "ce559c0c21410a5bb97508801fbff68c"
+"/packages/_fe_analyzer_shared/src/parser/parser.md": "9cc5da5eb7d930990cc9b1f176f29ed0"
 };
 
 self.addEventListener('activate', function (event) {
